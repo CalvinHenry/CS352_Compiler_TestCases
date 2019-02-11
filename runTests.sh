d9ebe6fd05f65e65f02618eb61aff49fa9f16c82
Calvin James Henry <henry89@sslab13.cs.purdue.edu>
2019-02-11 14:20:05 -0500

    Implmented failing test cases, added basic failing test

--- runTests.sh
+++ runTests.sh
@@ -10,4 +10,15 @@ do
         printf "Test Case %s Failed \n" $file
     fi
 done
+
+for file in f*.txt
+do
+    printf "Test Case: %s\n" $file
+    ../parser < $file
+    if [ $? -eq 0 ]; then
+        printf "Test Case %s Failed \n" $file
+    else
+        printf "Test Case %s Successful\n" $file
+    fi
+done
 exit 0
