a242534dd978d4c8b9f8805e0f303ccbc0e970a9
Calvin James Henry <henry89@borg04.cs.purdue.edu>
2019-02-13 17:21:15 -0500

    new tests

---
+++ p_6.java
@@ -0,0 +1,20 @@
+class Dog {
+	public static void main (String [] id) {
+            a = b;
+
+	}
+
+}
+
+class Fish {
+public t method(int a, t[][][] b){
+    int a;
+    
+
+    int b;
+    a = b;
+    b = c;
+    return true + false - 2 * 3/ (4 + +5);
+}
+}
+
