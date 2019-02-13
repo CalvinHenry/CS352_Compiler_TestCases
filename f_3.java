e30e3860b9b89206413d03ab8a63aa445a740278
Calvin James Henry <henry89@borg04.cs.purdue.edu>
2019-02-13 15:54:10 -0500

    updated test cases

---
+++ f_3.java
@@ -0,0 +1,20 @@
+class Dog {
+	public static void main (String [] id) {
+            a = b;
+
+	}
+
+}
+
+class Fish {
+public t method(int a, t[][][] b){
+    int a;
+    a = b;
+    b = c;
+    int b;
+    return true;
+
+
+}
+}
+
