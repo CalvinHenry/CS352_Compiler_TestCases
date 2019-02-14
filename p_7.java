307fd7d9fe968069b44ac7f838b069eed9379120
Calvin James Henry <henry89@sac03.cs.purdue.edu>
2019-02-14 14:38:57 -0500

    Some new test cases

---
+++ p_7.java
@@ -0,0 +1,23 @@
+class Dog {
+	public static void main (String [] id) {
+            {}
+
+	}
+
+}
+
+class Fish {
+public t method(int a, t[][][] b){
+    int a;
+
+    int b;
+    a = b;
+    b = c;
+    a = new int[][1];
+    return true + +false - 2 * 3/ (4 + +5);
+}
+    public s method(){
+        return this.a(a,b,c,d,e,f,g,h,i,j,k,l,(a + c) - - + - + - - + +(a * -b + 3)||4);
+    }
+}
+
