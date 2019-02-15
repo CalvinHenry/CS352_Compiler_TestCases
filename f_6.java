be13063e6b7db899df61c9371102c4b75363c605
Calvin James Henry <henry89@pod5-2.cs.purdue.edu>
2019-02-14 19:47:04 -0500

    Added another f test

---
+++ f_6.java
@@ -0,0 +1,10 @@
+class Dog {
+	public static void main (String [] id) {
+            {a = b;
+                System.out.println("String litteral
+With newline";
+                                   
+            }
+	}
+
+}
