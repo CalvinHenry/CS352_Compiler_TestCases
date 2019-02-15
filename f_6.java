5020f7cf99074afb95fd5e4e148e0a23c379c7b3
Calvin James Henry <henry89@pod5-2.cs.purdue.edu>
2019-02-14 19:49:04 -0500

    Fixed f_6

--- f_6.java
+++ f_6.java
@@ -2,7 +2,7 @@ class Dog {
 	public static void main (String [] id) {
             {a = b;
                 System.out.println("String litteral
-With newline";
+With newline");
                                    
             }
 	}
