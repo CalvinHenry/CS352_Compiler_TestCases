380b6d3d8b3368b3dd1e4a35bbfdb7aec3be58c8
Aaron Neustedter <aneusted@purdue.edu>
2019-03-04 14:27:15 -0500

    Small changes to test new grammer for proj 2. Major changes will be need to be made to make these tests correct for proj 2

--- p_dk1.java
+++ p_dk1.java
@@ -1,7 +1,8 @@
 class C {
 	public static void main (String [] id) {
 		{
-			System.out.println("SAFE STRING \n");
+			System.out.println("SAFE STRING");
+			System.out.print("Safe String without builtin newline\n");
 			System.out.println("String literal with escape chars \\ \" \\\\\n");
 		}
 	}
