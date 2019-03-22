9705358b28598f4bde349da59b44cfc411f92224
Calvin James Henry <henry89@pod5-3.cs.purdue.edu>
2019-03-22 16:37:57 -0400

    Added new expressions test case

---
+++ p_11.java
@@ -0,0 +1,18 @@
+class p_Dog {
+
+	public static void main (String [] id) {
+
+            {
+            System.out.println(24 * 34 * 23 / 3 * 3 /2 * (10 + 4 / 5 - 3 * 4 ) - 5 / 7 * 9 );
+            System.out.println(true && false || 3 + 2 * 30 / 2 >= 23 * 3 == false);
+            System.out.println(23 > 3);
+            System.out.println(3 < 3);
+            System.out.println(3 <= 3);
+            System.out.println(3 >= 23);
+            System.out.println(3 >= 23 == false);
+            }
+	}
+
+}
+
+
